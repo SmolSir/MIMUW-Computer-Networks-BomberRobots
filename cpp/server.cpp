a7ff1b42c4115c6b508f1d6e2e03030cc8964766
Bartosz Smolarczyk <bs429594@students.mimuw.edu.pl>
2022-06-10 01:11:08 +0200

    Update README, verifier and server progress

--- cpp/server.cpp
+++ cpp/server.cpp
@@ -9,7 +9,7 @@ using namespace std;
 namespace po = boost::program_options;
 
 /* -------------------------------------------------------------------------
-   Useful structures
+   Useful structures and declarations
    ------------------------------------------------------------------------- */
 /* structure for storing initial server params */
 struct Settings {
@@ -28,19 +28,25 @@ struct Settings {
 
 /* structure for storing game state */
 struct GameState {
+    bool is_active = false;
     uint16_t turn_number;
     map<PlayerId, Player> players;
     map<PlayerId, Position> robot_positions;
     map<PlayerId, Score> scores;
     map<BombId, Bomb> bombs;
     set<Position> blocks;
+
+    void reset();
 };
 
 /* structure for storing runtime data, game state and settings */
 struct Server {
     Settings settings;
     GameState game_state;
-    map<PlayerId, Player> connected_players;
+    PlayerId next_player_id;
+    BombId next_bomb_id;
+    
+    map<PlayerId, ClientMessageServer> players_messages;
     vector<AcceptedPlayer> accepted_players;
     vector<Turn> completed_turns;
 
@@ -48,12 +54,12 @@ struct Server {
     T random(T mod);
     Position random_position();
 
-    Hello hello();
-    AcceptedPlayer accepted_player(PlayerId &id, Player &player);
-    GameStarted game_started();
-    Turn initiate();
-    Turn turn();
-    GameEnded game_ended();
+    void reset_game_state();
+    Hello hello_message();
+    AcceptedPlayer add_accepted_player(Player &player);
+    Turn simulate_turn();
+    GameStarted start_game();
+    GameEnded end_game();
 };
 
 /* -------------------------------------------------------------------------
@@ -113,6 +119,15 @@ bool process_command_line(int argc, char **argv) {
     return true;
 }
 
+void GameState::reset() {
+    turn_number = 0;
+    players = { };
+    robot_positions = { };
+    scores = { };
+    bombs = { };
+    blocks = { };
+}
+
 template <Unsigned T>
 T Server::random(T mod) {
     minstd_rand random(settings.seed);
@@ -126,7 +141,19 @@ Position Server::random_position() {
     };
 }
 
-Hello Server::hello() {
+void Server::reset_game_state() {
+    game_state.reset();
+    next_bomb_id = 0;
+    next_player_id = 0;
+
+    players_messages = { };
+    accepted_players = { };
+    completed_turns = { };
+
+    return;
+}
+
+Hello Server::hello_message() {
     return Hello {
         .server_name = settings.server_name,
         .players_count = settings.players_count,
@@ -138,48 +165,64 @@ Hello Server::hello() {
     };
 }
 
-AcceptedPlayer Server::accepted_player(PlayerId &id, Player &player) {
-    return AcceptedPlayer {
-        .id = id,
+AcceptedPlayer Server::add_accepted_player(Player &player) {
+    game_state.players.insert({next_player_id, player});
+    
+    AcceptedPlayer accepted_player = {
+        .id = next_player_id,
         .player = player,
     };
+    accepted_players.push_back(accepted_player);
+    next_player_id++;
+
+    return accepted_player;
 }
 
-GameStarted Server::game_started() {
+GameStarted Server::start_game() {
+    game_state.is_active = true;
     return GameStarted {
         .players = game_state.players,
     };
 }
 
-Turn Server::initiate() {
-    game_state.turn_number = 0;
-    vector<Event> events = { };
-
-    for (auto const &[id, player] : game_state.players) {
-        Position position = random_position();
-        game_state.robot_positions.insert({id, position});
-        events.push_back(PlayerMoved {id, position});
-    }
-
-    for (uint16_t block = 0; block < settings.initial_blocks; block++) {
-        Position position = random_position();
-        game_state.blocks.insert(position);
-        events.push_back(BlockPlaced {position});
-    }
-
-    return Turn {
-        .turn = game_state.turn_number,
-        .events = events,
-    };
-}
-
-Turn Server::turn() {
+Turn Server::simulate_turn() {
     vector<Event> events = { };
     set<Position> blocks_destroyed = { };
     set<PlayerId> robots_destroyed = { };
+
+    Turn turn_result;
+
+    if (game_state.turn_number == 0) {
+        for (auto const &[id, player] : game_state.players) {
+            Position position = random_position();
+            game_state.robot_positions.insert({id, position});
+            events.push_back(PlayerMoved {id, position});
+        }
+
+        for (uint16_t block = 0; block < settings.initial_blocks; block++) {
+            Position position = random_position();
+            game_state.blocks.insert(position);
+            events.push_back(BlockPlaced {position});
+        }
+
+        turn_result = {
+            .turn = game_state.turn_number,
+            .events = events,
+        };
+    }
+    else { // game_state.turn_number > 0
+
+    }
+
+    game_state.turn_number++;
+    players_messages = { };
+    completed_turns.push_back(turn_result);
+
+    return turn_result;
 }
 
-GameEnded Server::game_ended() {
+GameEnded Server::end_game() {
+    game_state.is_active = false;
     return GameEnded {
         .scores = game_state.scores,
     };
